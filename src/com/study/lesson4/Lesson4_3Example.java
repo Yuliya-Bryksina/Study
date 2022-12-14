239fb3d9a8ea4c0e4dc96ee95f0e56dbd876ec7a
Yuliya-Bryksina <bakumovayv@gmail.com>
2022-12-14 19:17:10 +0300

    1

--- src/com/study/lesson4/Lesson4_3Example.java
+++ src/com/study/lesson4/Lesson4_3Example.java
@@ -7,13 +7,13 @@ public class Lesson4_3Example {
 
     public static void main(String[] args) {
         int month = 10;
-        if (month==12||month==1||month==2)
+        if (month == 12 || month == 1 || month == 2)
             System.out.println("Зима");
-        else if (month==3||month==4||month==5)
+        else if (month == 3 || month == 4 || month == 5)
             System.out.println("Весна");
-        else if (month==6||month==7||month==8)
+        else if (month ==6 || month ==7 || month == 8)
             System.out.println("Лето");
-        else if (month==9||month==10||month==11)
+        else
             System.out.println("Осень");
     }
 }
