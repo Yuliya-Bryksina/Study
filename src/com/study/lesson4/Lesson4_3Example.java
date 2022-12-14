392652cb163078eaac27fcbbd10ec066d0499075
Yuliya-Bryksina <bakumovayv@gmail.com>
2022-12-14 19:56:54 +0300

    1

--- src/com/study/lesson4/Lesson4_3Example.java
+++ src/com/study/lesson4/Lesson4_3Example.java
@@ -6,14 +6,16 @@ package com.study.lesson4;
 public class Lesson4_3Example {
 
     public static void main(String[] args) {
-        int month = 10;
+        int month = 4;
         if (month == 12 || month == 1 || month == 2)
             System.out.println("Зима");
-        else if (month == 3 || month == 4 || month == 5)
+        else if (month >= 3 && month <= 5)
             System.out.println("Весна");
-        else if (month ==6 || month ==7 || month == 8)
+        else if (month >= 6 && month <= 8)
             System.out.println("Лето");
-        else
+        else if (month >= 9 && month <= 11)
             System.out.println("Осень");
+        else
+            System.out.println("Неверный формат");
     }
 }
