51ad1be432482f75d808411b8d6f595bc4efacd9
Yuliya-Bryksina <bakumovayv@gmail.com>
2022-12-22 11:57:39 +0300

    task from the book

--- src/com/study/bookStudy/ContDemo.java
+++ src/com/study/bookStudy/ContDemo.java
@@ -5,7 +5,7 @@ public class ContDemo {
     public static void main(String[] args) {
         int i;
         for (i = 0; i < 100; i++) {
-            if ((i % 2) ==0) continue;
+            if ((i % 2) !=0) continue;
             System.out.println(i);
         }
     }
